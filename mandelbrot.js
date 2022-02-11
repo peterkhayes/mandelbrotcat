cf7e5c63c1f00a963336e6197c95a9bf550b4545
Peter Hayes <peter.k.hayes@gmail.com>
2022-02-11 08:22:48 -0800

    small tweaks

--- mandelbrot.js
+++ mandelbrot.js
@@ -145,12 +145,5 @@ $(document).on('ready', function() {
     render();
   });
 
-  $('body').append(
-  '<a href="http://hackreactor.com"> \
-  <img style="position: fixed; top: 0; right: 0; border: 0;" \
-  src="http://i.imgur.com/x86kKmF.png" \
-  alt="Built at Hack Reactor"> \
-  </a>');
-
   setTimeout(flashEyes, Math.random()*15000);
 });
